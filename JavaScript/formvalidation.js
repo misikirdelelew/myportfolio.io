b1b7566bd5c13969930d7ab5cbc421ee576f36bc
MishDM <mish@g.com>
2023-02-02 05:36:07 -0800

    form validation done with javascript

--- JavaScript/formvalidation.js
+++ JavaScript/formvalidation.js
@@ -3,11 +3,11 @@ const email = form.elements[1];
 const submit = document.querySelector('button[type=submit]');
 const messag = document.querySelector('form > span');
 submit.addEventListener('click', (event) => {
-    const { value } = email;
-    const loweredValue = value.toLowerCase();
-    if (value !== loweredValue) {
-        event.preventDefault();
-        messag.style.color = '#d8000c';
-        messag.innerHTML = 'Email should be written in lowercase letters.';
-    }
+  const { value } = email;
+  const loweredValue = value.toLowerCase();
+  if (value !== loweredValue) {
+    event.preventDefault();
+    messag.style.color = '#d8000c';
+    messag.innerHTML = 'Email should be written in lowercase letters.';
+  }
 });
